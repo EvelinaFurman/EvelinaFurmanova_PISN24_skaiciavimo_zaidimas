2d470c69627628ac21d38bc847fffd52a02f6642
Evelina Furmanova <evelina.furmanova@stud.viko.lt>
2024-10-02 21:49:37 +0300

    loops

--- main.cpp
+++ main.cpp
@@ -8,60 +8,98 @@
 
 using namespace std;
 
-int main()
-
-{
+int main() {
     string boarder = "****************************************";
     string task = "TASK NUMBER";
-    string thorns = "\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/";
     int choice;
+    int score = 0;
+    bool win = "YOU WIN";
+
+
     cout << boarder << endl;
     cout << "WELCOME TO MATH QUIZZ" << endl;
     cout << boarder << endl;
-    cout << thorns;
-    cout << "Please choose the category" <<endl;
-    do {
-        cout << "1. ADDITION\n2. SUBTRACTION\n3. DIVISION\n4. MULTIPLICATION\n5. QUIT\n";
-        cout <<"Enter you choice: ";
-        cin >> choice;
 
-        switch(choice)
-            {
-            case 1:
-                cout << "GREAT CHOISE";
+    cout << "Please choose the category" <<endl;
+    // do {
+    cout << "1. ADDITION\n2. SUBTRACTION\n3. DIVISION\n4. MULTIPLICATION\n5. QUIT\n";
+    cout <<"Enter you choice: ";
+    cin >> choice;
+    // int exit;
+    // exit = system("CLS");
+    // cin >> exit;
+
+    switch(choice)
+    {
+        case 1:
+            cout << "GREAT CHOISE" <<endl;
+
             cout << boarder <<endl;
             cout << task<< " 1" << endl;
             cout << boarder <<endl;
+
             int randomNumber1;
             int randomNumber2;
             int answer;
-            randomNumber1 = (rand() % 100 + 4);
-            randomNumber2 = (rand() % 50 + 4);
+
+            srand(time(nullptr));
+            randomNumber1 = rand() % 100 + 4;
+            randomNumber2 = rand() % 50 + 4;
             cout << randomNumber1 << " + " << randomNumber2 << " = " << endl;
             cin >> answer;
-            if (answer == randomNumber1 + randomNumber2)
-                cout << "YOU WIN" << endl;
+            if(answer == randomNumber1 + randomNumber2)
+                cout << win << endl;
             else
-                do {
+                if (answer != randomNumber1 + randomNumber2)
                     cout << "TRY AGAIN: ";
-                } while (answer == randomNumber1 + randomNumber2);
+            cin>>answer;
+            if(answer == randomNumber1 + randomNumber2)
+                cout << win << endl;
+            else
+                if(answer != randomNumber1 + randomNumber2)
+                    cout << "YOU LOSE" <<endl;
+            if(win == true)
+                cout << "SCORE +100 " << score << endl;
+            score = score + 100;
 
-            break;
-            case 2:
-                cout << "EXELLENT CHOISE";
-            break;
-            case 3:
-                cout << "WONDERFUL CHOISE";
-            break;
-            case 4:
-                cout << "LET'S GET STARTED";
-            break;
-            case 5:
-                cout << "GOODBYE";
-            return 0;
-            }
-        }while (choice != 5);
 
+        break;
+        case 2:
+        //     cout << "EXELLENT CHOISE" <<endl;
+        // int randomNumber1;
+        // int randomNumber2;
+        // int answer;
+        //
+        //
+        // srand(time(nullptr));
+        // randomNumber1 = rand() % 100 + 4;
+        // randomNumber2 = rand() % 50 + 4;
+        // cout << randomNumber1 << " + " << randomNumber2 << " = " << endl;
+        // cin >> answer;
+        // if (answer == randomNumber1 + randomNumber2)
+        //     cout << "YOU WIN" << endl;
+        // else
+        //     if(answer != randomNumber1 - randomNumber2)
+        //         cout << "YOU HAVE ONE MORE TRY";
+        //         cin >> answer;
+        // if(answer == randomNumber1 + randomNumber2)
+
+
+
+
+        break;
+        case 3:
+            cout << "WONDERFUL CHOISE";
+        break;
+        case 4:
+            cout << "LET'S GET STARTED";
+        break;
+        case 5:
+            cout << "GOODBYE";
+        return 0;
+    }
+    // }while (choice != 5);
+}
 
     // cout << boarder <<endl;
     // cout << task<< " 1" << endl;
@@ -97,33 +135,33 @@ int main()
     //         }while(atsakymas1 != number11+number12);
     //              cout << "YOU DID IT!" << endl;
 
-    cout << boarder << endl;
-    cout << task <<"2" <<endl;
-    cout << boarder<< endl;
-
-    srand(time(nullptr));
-    int number21 = rand() % 101 +2;
-    srand(time(nullptr));
-    int number22 = rand() % 10 +2;
-    cout << number21<<" + " << number22 << " = ";
-    int atsakymas2;
-    cin >> atsakymas2;
-        if (atsakymas2 == number21 + number22)
-            cout << "GOOD JOB";
-        else
-            do {
-                cout << "TRY AGAIN: ";
-                cin >> atsakymas2;
-            }while(atsakymas2 != number21 + number22);
-    cout << boarder << endl;
-    cout << task<< "3" <<endl;
-    cout << boarder << endl;
-    srand(time(nullptr));
-    int number5 = rand() % 101 +2;
-    srand(time(nullptr));
-    int number6 = rand() % 10 +2;
-    cout << number5<<" + " << number6 << " = ";
-    int atsakymas3;
-
-    return 0;
-}
+//     cout << boarder << endl;
+//     cout << task <<"2" <<endl;
+//     cout << boarder<< endl;
+//
+//     srand(time(nullptr));
+//     int number21 = rand() % 101 +2;
+//     srand(time(nullptr));
+//     int number22 = rand() % 10 +2;
+//     cout << number21<<" + " << number22 << " = ";
+//     int atsakymas2;
+//     cin >> atsakymas2;
+//         if (atsakymas2 == number21 + number22)
+//             cout << "GOOD JOB";
+//         else
+//             do {
+//                 cout << "TRY AGAIN: ";
+//                 cin >> atsakymas2;
+//             }while(atsakymas2 != number21 + number22);
+//     cout << boarder << endl;
+//     cout << task<< "3" <<endl;
+//     cout << boarder << endl;
+//     srand(time(nullptr));
+//     int number5 = rand() % 101 +2;
+//     srand(time(nullptr));
+//     int number6 = rand() % 10 +2;
+//     cout << number5<<" + " << number6 << " = ";
+//     int atsakymas3;
+//
+//     return 0;
+// }
