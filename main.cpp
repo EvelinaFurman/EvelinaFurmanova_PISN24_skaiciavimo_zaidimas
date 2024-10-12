e52b673dc002394ace4a9ab9bf11c3b05e2187a3
Evelina Furmanova <evelina.furmanova@stud.viko.lt>
2024-10-12 16:38:21 +0300

    working menu loop

--- main.cpp
+++ main.cpp
@@ -5,163 +5,197 @@
 #include <random>
 #include <string.h>
 #include <string>
+#include <bits/stl_algo.h>
 
 using namespace std;
 
 int main() {
     string boarder = "****************************************";
-    string task = "TASK NUMBER";
+    string add = "ADDITION";
+    string div = "DIVISION";
+    string mult = "MULTIPLICATION";
+    string subt = "SUBTRACTION";
     int choice;
     int score = 0;
-    bool win = "YOU WIN";
+    string wrongAnswer = "GOOD LUCK NEXT TIME";
+    string rightAnswer = "GOOD JOB";
+    int randomNumber1;
+    int randomNumber2;
+    long answer;
+    short task = 0;
 
 
     cout << boarder << endl;
     cout << "WELCOME TO MATH QUIZZ" << endl;
     cout << boarder << endl;
-
-    cout << "Please choose the category" <<endl;
-    // do {
-    cout << "1. ADDITION\n2. SUBTRACTION\n3. DIVISION\n4. MULTIPLICATION\n5. QUIT\n";
-    cout <<"Enter you choice: ";
-    cin >> choice;
-    // int exit;
-    // exit = system("CLS");
-    // cin >> exit;
-
-    switch(choice)
+    while(choice != 5)
     {
-        case 1:
-            cout << "GREAT CHOISE" <<endl;
-
-            cout << boarder <<endl;
-            cout << task<< " 1" << endl;
-            cout << boarder <<endl;
-
-            int randomNumber1;
-            int randomNumber2;
-            int answer;
-
-            srand(time(nullptr));
-            randomNumber1 = rand() % 100 + 8;
-            randomNumber2 = rand() % 50 + 6;
-            cout << randomNumber1 << " + " << randomNumber2 << " = " << endl;
-            cin >> answer;
-            if(answer == randomNumber1 + randomNumber2)
-                cout << win << endl;
-            else
-                if (answer != randomNumber1 + randomNumber2)
-                    cout << "TRY AGAIN: ";
-            cin>>answer;
-            if(answer == randomNumber1 + randomNumber2)
-                cout << win << endl;
-            else
-                if(answer != randomNumber1 + randomNumber2)
-                    cout << "YOU LOSE" <<endl;
-            if(win == true)
-                cout << "SCORE +100 " << score << endl;
-            score = score + 100;
-
-
-        break;
-        case 2:
-        //     cout << "EXELLENT CHOISE" <<endl;
-        // int randomNumber1;
-        // int randomNumber2;
-        // int answer;
-        //
-        //
-        // srand(time(nullptr));
-        // randomNumber1 = rand() % 100 + 4;
-        // randomNumber2 = rand() % 50 + 4;
-        // cout << randomNumber1 << " + " << randomNumber2 << " = " << endl;
-        // cin >> answer;
-        // if (answer == randomNumber1 + randomNumber2)
-        //     cout << "YOU WIN" << endl;
-        // else
-        //     if(answer != randomNumber1 - randomNumber2)
-        //         cout << "YOU HAVE ONE MORE TRY";
-        //         cin >> answer;
-        // if(answer == randomNumber1 + randomNumber2)
+        cout << "Please choose the category" <<endl;
+        cout << "1. ADDITION\n2. DIVISION\n3. SUBTRACTION\n4. MULTIPLICATION\n5. QUIT\n";
+        cout <<"Enter you choice: ";
+        cin >> choice;
 
 
 
 
-        break;
-        case 3:
-            cout << "WONDERFUL CHOISE";
-        break;
-        case 4:
-            cout << "LET'S GET STARTED";
-        break;
-        case 5:
-            cout << "GOODBYE";
-        return 0;
+        switch(choice)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+        if (choice == 1) {
+            cout << "   Great choise!\n Now solve 10 addition equations" <<endl;
+            cout << boarder << endl;
+            cout <<add << endl;
+            cout <<boarder << endl;
+
+            for(int i = 1; i <= 10; i++) {
+                srand(time(nullptr));
+                randomNumber1 = (rand() % 100 + 8);
+                randomNumber2 = (rand() % 50 + 6);
+                cout << randomNumber1 << " + " << randomNumber2 << " = ";
+                cin >> answer;
+                if(answer == randomNumber1 + randomNumber2)
+                    cout << rightAnswer <<endl;
+                else
+                    cout << wrongAnswer <<endl;
+                if (answer == randomNumber1 + randomNumber2)
+                    score++;
+                else
+                    score = score-0;
+                if(answer == randomNumber1 + randomNumber2)
+                    task++;
+                else
+                    task++;
+            }
+            cout << " "<<endl;
+            cout << "CONGRATULATIONS!!!\n You scored: " <<score << endl;
+            cout << "Out of " << task <<endl;
+            cout << " " <<endl;
+            cout << boarder << endl;
+            cout << boarder << endl;
+        }
+        if(task>10)
+            system("cls");
+
+        if(choice == 2) {
+            cout << boarder << endl;
+            cout << "    Excellent choice!!!\n Now solve 10 division equations" << endl;
+            cout << boarder << endl;
+            cout <<div << endl;
+            cout << boarder << endl;
+            cout << "*rouns down your answer before entering" << endl;
+
+            for(int i = 1; i <= 10; i++)
+            {
+                srand(time(nullptr));
+                randomNumber1 = (rand() % 63 + 9);
+                randomNumber2 = (rand() % 9 + 1);
+                cout << randomNumber1 << " / " << randomNumber2 << " = ";
+                cin >> answer;
+                if(lround(answer) == randomNumber1 / randomNumber2)
+                    cout << rightAnswer <<endl;
+                else
+                    cout << wrongAnswer <<endl;
+                if(lround(answer) == randomNumber1 / randomNumber2)
+                    score++;
+                else
+                    score=(score-0);
+                if(lround(answer) == randomNumber1 / randomNumber2)
+                    task++;
+                else
+                    task++;
+            }
+            cout << " "<<endl;
+            cout << "CONGRATULATIONS!!!\n You scored: " <<score << endl;
+            cout << "Out of "<<task<<endl;
+            cout << " " <<endl;
+            cout << boarder << endl;
+            cout << boarder << endl;
+
+        }
+        if(task>10)
+        system("cls");
+
+        if(choice == 3) {
+            cout << "    Perfect choice\n Now solve 10 subtraction problems" << endl;
+            cout << boarder << endl;
+            cout <<  subt << endl;
+            cout << boarder << endl;
+
+            for(int i = 1; i <= 10; i++) {
+                srand(time(nullptr));
+                randomNumber1 = (rand() % 100 + 31);
+                randomNumber2 = (rand() % 50 + 1);
+                cout << randomNumber1 << " - " << randomNumber2 << " = ";
+                cin >> answer;
+                if(answer == randomNumber1 - randomNumber2)
+                    cout << rightAnswer <<endl;
+                else
+                    cout << wrongAnswer <<endl;
+                if(answer == randomNumber1 - randomNumber2)
+                    score++;
+                else
+                    score=(score-0);
+                if(answer == randomNumber1 - randomNumber2)
+                    task++;
+                else
+                    task++;
+            }
+            cout << " "<<endl;
+            cout << "CONGRATULATIONS!!!\n You scored: " <<score << endl;
+            cout << "Out of " << task <<endl;
+            cout << " " <<endl;
+            cout << boarder << endl;
+            cout << boarder << endl;
+
+        }
+        if(task>10)
+            system("cls");
+
+        if(choice == 4) {
+            cout << "   Let's go!!!\n Now solve 10 multiplivation equations" <<endl;
+            cout << boarder << endl;
+            cout << mult << endl;
+            cout << boarder << endl;
+            for(int i = 1; i <= 10; i++) {
+                srand(time(nullptr));
+                randomNumber1 = (rand() % 100 + 9);
+                randomNumber2 = (rand() % 50 + 1);
+                cout << randomNumber1 << " * " << randomNumber2 << " = ";
+                cin >> answer;
+                if(answer == randomNumber1 * randomNumber2)
+                    cout << rightAnswer <<endl;
+                else
+                    cout << wrongAnswer <<endl;
+                if(answer == randomNumber1 * randomNumber2)
+                    score++;
+                else
+                    score=(score-0);
+                if(answer == randomNumber1 * randomNumber2)
+                    task++;
+                else
+                    task++;
+            }
+            cout << " "<< endl;
+            cout << "CONGRATULATIONS!!!\n You scored: " <<score << endl;
+            cout << "Out of " << task <<endl;
+        }
+        if(task>10)
+            system("cls");
+        if(choice == 5) {
+            cout << boarder << endl;
+            cout << " " << endl;
+            cout << " WE ARE SORRY TO HEAR THAT YOU ARE LEAVING\n WAITING YOU TO COME BACK\n HAVE A NICE DAY"<<endl;
+        }
+
     }
-    // }while (choice != 5);
 }
-
-    // cout << boarder <<endl;
-    // cout << task<< " 1" << endl;
-    // cout << boarder <<endl;
-    // int randomNumber1;
-    // int randomNumber2;
-    // int answer;
-    // randomNumber1 = (rand() % 100 + 4);
-    // randomNumber2 = (rand() % 50 + 4);
-    // cout << randomNumber1 << " + " << randomNumber2 << " = " << endl;
-    // cin >> answer;
-    // if (answer == randomNumber1 + randomNumber2)
-    //     cout << "YOU WIN" << endl;
-    // else
-    //     do {
-    //         cout << "TRY AGAIN: ";
-    //     } while (answer == randomNumber1 + randomNumber2);
-
-    // srand(time(nullptr));
-    // int number11 = rand() % 100;
-    // srand(time(nullptr));
-    // int number12 = rand() % 50 +1;
-    //     cout << number11<<" + " << number12 << " = ";
-    // int atsakymas1;
-    //     cin >> atsakymas1;
-    //
-    //     if(atsakymas1 == number11+number12)
-    //         cout << "GOOD JOB"<<endl;
-    //     else
-    //         do {
-    //             cout <<"TRY AGAIN: ";
-    //             cin >> atsakymas1;
-    //         }while(atsakymas1 != number11+number12);
-    //              cout << "YOU DID IT!" << endl;
-
-//     cout << boarder << endl;
-//     cout << task <<"2" <<endl;
-//     cout << boarder<< endl;
-//
-//     srand(time(nullptr));
-//     int number21 = rand() % 101 +2;
-//     srand(time(nullptr));
-//     int number22 = rand() % 10 +2;
-//     cout << number21<<" + " << number22 << " = ";
-//     int atsakymas2;
-//     cin >> atsakymas2;
-//         if (atsakymas2 == number21 + number22)
-//             cout << "GOOD JOB";
-//         else
-//             do {
-//                 cout << "TRY AGAIN: ";
-//                 cin >> atsakymas2;
-//             }while(atsakymas2 != number21 + number22);
-//     cout << boarder << endl;
-//     cout << task<< "3" <<endl;
-//     cout << boarder << endl;
-//     srand(time(nullptr));
-//     int number5 = rand() % 101 +2;
-//     srand(time(nullptr));
-//     int number6 = rand() % 10 +2;
-//     cout << number5<<" + " << number6 << " = ";
-//     int atsakymas3;
-//
-//     return 0;
-// }
